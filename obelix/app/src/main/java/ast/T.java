52b4a64586af729b5ccfe96a75bda7a193e86fde
sergiodguezc <sergiodguezc@gmailcom>
2022-03-24 14:10:45 +0100

    Cambios ast

---
+++ obelix/app/src/main/java/ast/T.java
@@ -0,0 +1,8 @@
+package ast;
+
+public abstract class T implements ASTNode {
+    public abstract KindE kind();
+    public NodeKind nodeKind() {return NodeKind.TIPO;}
+    public String toString() {return "";}
+
+}
