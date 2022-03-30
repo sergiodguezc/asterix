cb035a23bab0e78e6738e25eb797dbc5254f7386
sergiodguezc <sergiodguezc@gmailcom>
2022-03-30 21:17:07 +0200

    Errores primer intento

--- obelix/src/main/java/errors/GestionErroresAsterix.java
+++ obelix/src/main/java/errors/GestionErroresAsterix.java
@@ -4,14 +4,14 @@ import alexop.UnidadLexica;
 
 public class GestionErroresAsterix {
    public void errorLexico(int fila, int columna, String lexema) {
-     System.out.println("ERROR fila "+fila+" columna "+columna+": Caracter inesperado: "+lexema); 
+       System.err.println("***ERROR lexico. Fila: " + fila + ", Columna: " + columna + " Elemento inesperado \"" + lexema+"\"");
      System.exit(1);
    }  
    public void errorSintactico(UnidadLexica unidadLexica) {
      if (unidadLexica.lexema() != null) {
-       System.out.println("ERROR fila "+unidadLexica.fila()+" columna "+unidadLexica.columna()+": Elemento inesperado \""+unidadLexica.lexema()+"\"");
+       System.err.println("***ERROR sintactico. Fila: " + unidadLexica.fila() + ", Columna: " + unidadLexica.columna() + " Elemento inesperado \"" + unidadLexica.lexema()+"\"");
      } else {
-       System.out.println("ERROR fila "+unidadLexica.fila()+" columna "+unidadLexica.columna()+": Elemento inesperado");
+       System.err.println("***ERROR sintactico. Fila: " + unidadLexica.fila() + ", Columna: " + unidadLexica.columna() + " Elemento inesperado " + unidadLexica.lexema());
      }
      System.exit(1);
    }
